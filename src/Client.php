128e20bfdb748aca482c7653aa6b43be2c46dece
Ilias P <chinchillabrains@gmail.com>
2022-11-01 21:32:10 +0200

    Initial commit

---
+++ src/Client.php
@@ -0,0 +1,49 @@
+<?php namespace Chinchillabrains\Eltaws;
+
+class Client{
+
+    private $serviceLocation = 'http://212.205.47.226:9003';
+    
+    private $wsdir = __DIR__ . '/ws/';
+
+    public function sendPackage( array $packageInfo ): array
+    {
+        $service = 'CREATEAWB.wsdl';
+        return $this->sendRequest( $service, $packageInfo );
+    }
+
+    public function getLabel( array $packageInfo ): array
+    {
+        $service = 'PELB64VG.wsdl';
+        return $this->sendRequest( $service, $packageInfo );
+    }
+
+    public function getTrackingData( array $packageInfo ): array
+    {
+        $service = 'PELTT01.wsdl';
+        return $this->sendRequest( $service, $packageInfo );
+    }
+
+    public function getStoreByZip( string $zip ): array
+    {
+        $service = 'PELTT01.wsdl';
+        return $this->sendRequest( $service, $zip );
+    }
+
+    private function sendRequest( string $service, array $requestData )
+    {
+        $client = new SoapClient( 
+            $this->wsdir . $service,
+            [ 'location' => $this->$serviceLocation ] 
+        );
+        try {
+            return $client->READ( $requestData );
+        } catch ( SoapFault $e ) {
+            error_log( $e );
+            return [
+                'error' => $e
+            ];
+        }
+    }
+
+}
--- src/Main.php
+++
@@ -1,5 +0,0 @@
-<?php namespace Chinchillabrains\Eltaws;
-
-class Main{
-
-}
