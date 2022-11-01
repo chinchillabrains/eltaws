5e4033e664ae5747eb658453b66505570addfe65
Ilias P <chinchillabrains@gmail.com>
2022-11-01 20:31:42 +0200

    Add base files

---
+++ src/Main.php
@@ -0,0 +1,5 @@
+<?php namespace Chinchillabrains\Eltaws;
+
+class Main{
+
+}
---
+++ tests/EltaTests.php
@@ -0,0 +1,8 @@
+<?php  namespace Chinchillabrains\Eltaws\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class EltaTests extends TestCase
+{
+  
+}
